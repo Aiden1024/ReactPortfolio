a2a5f465adabf86a3907dd1a346fede4bcaec992
Aiden1024 <Aiden1024>
2023-08-18 11:10:55 +0800

    finsih about page

---
+++ tailwindreact-app/src/components/About.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div name="About" className=' w-full h-screen bg-gradient-to-b
+     from-gray-800 to-black text-white'>
+      <div className=' max-w-screen-lg p-4 mx-auto flex flex-col
+       justify-center w-full h-full'>
+
+        <div className=' pb-8'>
+            <p className=' text-4xl font-bold inline border-b-4
+             border-gray-500'>About</p>
+        </div>
+            <p className=' text-xl mt-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, modi sunt doloremque ex enim deserunt, dolor vel veritatis unde tempore officia architecto eos ea nostrum velit dolorum itaque minus aperiam consequatur odit nam porro? Enim porro velit officia laborum tempore obcaecati? Quis quas deserunt eaque, adipisci accusamus corrupti expedita cum!</p>
+
+            <br />
+
+            <p className=' text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit necessitatibus, voluptatibus non expedita nihil animi incidunt tempora corporis quas exercitationem. Quae molestias recusandae neque tempora nesciunt optio harum voluptatibus eaque et. Quam illum aliquam aut, ipsam a fugiat nemo explicabo quae rem cupiditate obcaecati quasi deserunt alias eos consequuntur. Perferendis.</p>
+
+
+        
+      </div>
+    </div>
+  )
+}
+
+export default About
--- tailwindreact-app/src/components/Home.jsx
+++ tailwindreact-app/src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     // To transfer screen template, mobile: flex-col, pc: md:flex-row
     <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
       <div className='max-w-screen-lg mx-auto flex md:flex-row 
-      items-center flex-col justify-center h-full px-4 text-white'>
+      items-center flex-col justify-center h-full px-4 text-white '>
         <div className='flex flex-col justify-center h-full'>
             <h2 className='text-4xl sm:text-7xl font-bold text-white'>
                 I am a Front Developer
